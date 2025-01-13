ba13188b0f8c2e723819fbaa16b9e9889d922f2c
Lucel Da Silva <luceldasilva@yahoo.com>
2025-01-12 21:52:56 -0300

    🔨 (code) añadir funcionalidad de actualizar skills
    para su tipo y que personaje usa

---
+++ queries_db/update_skills.py
@@ -0,0 +1,29 @@
+from queries_db import pg_engine
+from sqlalchemy import MetaData as md, Table, update
+import pandas as pd
+import pyprojroot
+
+
+root_path = pyprojroot.here()
+data_path = root_path / 'etl' / 'pentaho' / 'output'
+
+connection = pg_engine.connect()
+metadata = md()
+skills = Table('skills', metadata, autoload_with=connection)
+
+
+df = pd.read_csv(data_path.joinpath('actualizar_los_dos.csv'))
+
+for index, row in df.iterrows():
+    stmt = (
+        update(skills).
+        where(skills.c.skill_id == row['skill_id']).
+        values(
+            skill_type_id=row['skill_type_id'],
+            character_id=row['character_id']
+        )
+    )
+    connection.execute(stmt)
+
+
+connection.close()
