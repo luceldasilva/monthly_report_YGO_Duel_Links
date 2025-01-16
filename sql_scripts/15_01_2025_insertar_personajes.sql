5977eb8c506c5d42f5521ea0ffa7daffb8e7002b
Lucel Da Silva <luceldasilva@yahoo.com>
2025-01-15 22:02:54 -0300

    🚧 (refactor) actualizacion de la dimension skill

---
+++ sql_scripts/15_01_2025_insertar_personajes.sql
@@ -0,0 +1,9 @@
+INSERT INTO characters (name_character, serie_id) VALUES ('Yuri', 6);
+INSERT INTO characters (name_character, serie_id) VALUES ('Sherry LeBlanc', 3);
+INSERT INTO characters (name_character, serie_id) VALUES ('Jesse Anderson', 2);
+INSERT INTO characters (name_character, serie_id) VALUES ('Yugo', 6);
+INSERT INTO characters (name_character, serie_id) VALUES ('Rin', 6);
+INSERT INTO characters (name_character, serie_id) VALUES ('Mizar', 5);
+INSERT INTO characters (name_character, serie_id) VALUES ('Yami Marik', 1);
+INSERT INTO characters (name_character, serie_id) VALUES ('Playmaker y Ai', 7);
+INSERT INTO characters (name_character, serie_id) VALUES ('Reginald Shark Kastle', 5);
---
+++ sql_scripts/insert_unknown_values.sql
@@ -0,0 +1,2 @@
+INSERT INTO series (serie_id, serie) VALUES (-1, 'Multiverso');
+INSERT INTO characters (character_id, name_character, serie_id) VALUES (-1, 'Arquetipo del Juego Físico', -1);
