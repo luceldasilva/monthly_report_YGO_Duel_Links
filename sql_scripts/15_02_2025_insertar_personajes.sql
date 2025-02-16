8e3820bf1d7e40dcc7e81af34ede9094c389bc22
Lucel Da Silva <luceldasilva@yahoo.com>
2025-02-16 00:39:51 -0300

    🧐 (data) agregar nuevas skills con sus personajes y nuevos personajes

---
+++ sql_scripts/15_02_2025_insertar_personajes.sql
@@ -0,0 +1,10 @@
+INSERT INTO characters (name_character, serie_id) VALUES ('Anna Kaboom', 5);
+INSERT INTO characters (name_character, serie_id) VALUES ('Zane Truesdale', 2);
+INSERT INTO characters (name_character, serie_id) VALUES ('Shay Obsidian', 6);
+INSERT INTO characters (name_character, serie_id) VALUES ('Dr. Vellian Crowler', 2);
+INSERT INTO characters (name_character, serie_id) VALUES ('Yami Bakura', 1);
+INSERT INTO characters (name_character, serie_id) VALUES ('Maximillion Pegasus', 1);
+INSERT INTO characters (name_character, serie_id) VALUES ('Yugi Muto DM', 1);
+INSERT INTO characters (name_character, serie_id) VALUES ('Sora Perse', 6);
+INSERT INTO characters (name_character, serie_id) VALUES ('Kite Tenjo', 5);
+INSERT INTO characters (name_character, serie_id) VALUES ('The Gore', 7);
