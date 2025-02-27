dac4658e6ae58294de3045093760269c9f31e209
Lucel Da Silva <luceldasilva@yahoo.com>
2025-02-26 21:30:52 -0300

    ✨ (feat) agregar conteo de mazos distintos
    al lado del conteo de registros y comparar el mes anterior

--- visualization/comparison_two_months.py
+++ visualization/comparison_two_months.py
@@ -1,11 +1,11 @@
 import pandas as pd
-import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from queries_db.constants import tables_db
 from queries_db import dataframe_queries as dfq
 
 
 def indicator(
-    kog_df: pd.DataFrame, month_fact_table: str, year_fact_table: str
+    kog_df: pd.DataFrame, decks_sum: pd.DataFrame
 ):
     kog_previous_df = dfq.df_query(
         tables_db[-2], tables_db[-2][-3:]
@@ -13,18 +13,29 @@ def indicator(
     
     count_kog_previous_df = len(kog_previous_df)
 
-    fig = go.Figure()
-
-    fig.add_trace(go.Indicator(
-        mode = "number+delta",
-        title = {'text': f'<br>Registros de {month_fact_table} {year_fact_table}</br><span style="font-size:0.8em;color:gray">Comparación al mes anterior</span>'},
-        value = len(kog_df),
-        delta = {
+    fig = make_subplots(
+        rows=1, cols=2,
+        specs=[[{'type': 'indicator'}, {'type': 'indicator'}]]
+    )
+    
+    
+    fig.add_indicator(
+        mode="number",
+        value=len(decks_sum),
+        title="Mazos<br>distintos",
+        row=1, col=1
+    )
+    
+    fig.add_indicator(
+        mode="number+delta",
+        value=len(kog_df),
+        title="Registros",
+        delta={
             'reference': count_kog_previous_df,
             'relative': True,
-            'position' : "top",
             'valueformat': ".1%"
-        })
+        },
+        row=1, col=2
     )
 
     fig.show()
