99ef9896b22197e5a73963aa37663e6f922edfec
Lucel Da Silva <luceldasilva@yahoo.com>
2025-04-15 16:33:30 -0300

    🚧 (code) actualizar personajes y sus habilidades

---
+++ sql_scripts/15_04_2025_insertar_personajes.sql
@@ -0,0 +1,6 @@
+INSERT INTO characters (name_character, serie_id) VALUES ('Jack Atlas', 3);
+INSERT INTO characters (name_character, serie_id) VALUES ('Quattro', 5);
+INSERT INTO characters (name_character, serie_id) VALUES ('Declan Akaba', 6);
+INSERT INTO characters (name_character, serie_id) VALUES ('Yusei Fudo', 3);
+INSERT INTO characters (name_character, serie_id) VALUES ('Akira Zaizen', 7);
+INSERT INTO characters (name_character, serie_id) VALUES ('Rex Raptor', 1);
---
+++ sql_scripts/15_04_2025_kc_cup_actualizar_tipo_y_personaje.sql
@@ -0,0 +1,10 @@
+UPDATE skills SET skill_type_id = 1, character_id = 40 WHERE skill_id = 92;
+UPDATE skills SET skill_type_id = 1, character_id = 32 WHERE skill_id = 93;
+UPDATE skills SET skill_type_id = 2, character_id = -1 WHERE skill_id = 94;
+UPDATE skills SET skill_type_id = 1, character_id = 37 WHERE skill_id = 95;
+UPDATE skills SET skill_type_id = 1, character_id = 38 WHERE skill_id = 96;
+UPDATE skills SET skill_type_id = 1, character_id = 16 WHERE skill_id = 97;
+UPDATE skills SET skill_type_id = 1, character_id = 42 WHERE skill_id = 98;
+UPDATE skills SET skill_type_id = 2, character_id = -1 WHERE skill_id = 99;
+UPDATE skills SET skill_type_id = 1, character_id = 38 WHERE skill_id = 100;
+UPDATE skills SET skill_type_id = 1, character_id = 41 WHERE skill_id = 101;
