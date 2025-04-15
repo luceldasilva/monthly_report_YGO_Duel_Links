d6c6b3797b8d21db151851b0dadfcf3bb185b75c
Lucel Da Silva <luceldasilva@yahoo.com>
2025-04-15 16:17:40 -0300

    ✨ (feat) nueva tabla de copa kc

---
+++ sql_scripts/create_kc_cup_abr_fact_table.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS kc_cup_2025_april (
+    id SERIAL PRIMARY KEY,
+    player_id INT NOT NULL,
+    deck_id INT NOT NULL,
+    skill_id INT NOT NULL,
+    date_id INT NOT NULL,
+    zerotg BOOLEAN NOT NULL,
+    zephra BOOLEAN NOT NULL,
+    bryan BOOLEAN NOT NULL,
+    xenoblur BOOLEAN NOT NULL,
+    yamiglen BOOLEAN NOT NULL,
+    latino_vania BOOLEAN NOT NULL,
+    updater_label VARCHAR(32) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT fk_kc_cup_2025_feb_player_id 
+        FOREIGN KEY (player_id) 
+        REFERENCES players (player_id),
+    CONSTRAINT fk_kc_cup_2025_feb_deck_id 
+        FOREIGN KEY (deck_id) 
+        REFERENCES decks (deck_id),
+    CONSTRAINT fk_kc_cup_2025_feb_skill_id
+        FOREIGN KEY (skill_id)
+        REFERENCES skills (skill_id),
+	CONSTRAINT fk_kc_cup_2025_feb_date_id
+        FOREIGN KEY (date_id)
+        REFERENCES calendar_2025 (date_id)
+);
+
+
+GRANT SELECT, INSERT, UPDATE, TRUNCATE, REFERENCES, TRIGGER ON kc_cup_2025_april TO usuario_que_usas;
+
+ALTER TABLE kc_cup_2025_april OWNER TO usuario_que_usas;
---
+++ sql_scripts/formatear_permisos_solo_lectura_no_etl.sql
@@ -0,0 +1,5 @@
+REVOKE ALL ON tabla FROM usuario_que_usas;
+
+GRANT SELECT ON tabla TO usuario_que_usas;
+
+GRANT CREATE ON SCHEMA public TO usuario_que_usas;
