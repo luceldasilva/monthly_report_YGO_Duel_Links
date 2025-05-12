7daa53a414c912dd8b3e0b405294a1572d5203d1
Lucel Da Silva <luceldasilva@yahoo.com>
2025-05-12 20:43:27 -0300

    🧐 (data) agregar skills nuevas

---
+++ sql_scripts/12_05_2025_actualizar_tipo_y_personaje.sql
@@ -0,0 +1,6 @@
+UPDATE skills SET skill_type_id = 1, character_id = 17 WHERE skill_id = 102;
+UPDATE skills SET skill_type_id = 1, character_id = 17 WHERE skill_id = 103;
+UPDATE skills SET skill_type_id = 2, character_id = -1 WHERE skill_id = 104;
+UPDATE skills SET skill_type_id = 1, character_id = 29 WHERE skill_id = 105;
+UPDATE skills SET skill_type_id = 1, character_id = 36 WHERE skill_id = 106;
+UPDATE skills SET skill_type_id = 1, character_id = 36 WHERE skill_id = 107;
