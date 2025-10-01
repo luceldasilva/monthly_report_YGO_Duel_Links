482844c6f5c153bc87ff586ad8cc4b5258bdadce
Lucel Da Silva <luceldasilva@yahoo.com>
2025-10-01 16:00:36 -0300

    🔖 (tag) v.1.1.1.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def readme() -> str:
 
 setup(
     name='monthly_report_KOG_Duel_Links',
-    version='1.1.1',
+    version='1.1.1.1',
     python_requires='>=3',
     packages=find_packages(),
     long_description=readme()
